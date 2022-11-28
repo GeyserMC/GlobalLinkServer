f8f83cf2e5ff6105ca607dee38724d29ead63170
Konicai <71294714+Konicai@users.noreply.github.com>
2022-11-28 18:49:41 -0500

    Bedrock_v545 is not available

--- src/main/java/org/geysermc/globallinkserver/bedrock/util/BedrockVersionUtils.java
+++ src/main/java/org/geysermc/globallinkserver/bedrock/util/BedrockVersionUtils.java
@@ -40,6 +40,7 @@ import com.nukkitx.protocol.bedrock.v503.Bedrock_v503;
 import com.nukkitx.protocol.bedrock.v527.Bedrock_v527;
 import com.nukkitx.protocol.bedrock.v534.Bedrock_v534;
 import com.nukkitx.protocol.bedrock.v544.Bedrock_v544;
+import com.nukkitx.protocol.bedrock.v545.Bedrock_v545;
 import com.nukkitx.protocol.bedrock.v554.Bedrock_v554;
 import com.nukkitx.protocol.bedrock.v557.Bedrock_v557;
 import com.nukkitx.protocol.bedrock.v560.Bedrock_v560;
@@ -76,10 +77,7 @@ public class BedrockVersionUtils {
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v527.V527_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v534.V534_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v544.V544_CODEC);
-        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v544.V544_CODEC.toBuilder()
-                .minecraftVersion("1.19.21")
-                .protocolVersion(545)
-                .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v545.V545_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v554.V554_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v557.V557_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(LATEST_CODEC);
