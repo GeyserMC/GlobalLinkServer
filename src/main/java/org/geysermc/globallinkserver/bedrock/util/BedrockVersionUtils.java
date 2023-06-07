bcfafebde990bce1d2aa49ee30e4591498d562da
Konicai <71294714+Konicai@users.noreply.github.com>
2023-06-07 13:12:30 -0400

    588 -> 589 and bump mcpl

--- src/main/java/org/geysermc/globallinkserver/bedrock/util/BedrockVersionUtils.java
+++ src/main/java/org/geysermc/globallinkserver/bedrock/util/BedrockVersionUtils.java
@@ -48,7 +48,7 @@ import org.cloudburstmc.protocol.bedrock.codec.v567.Bedrock_v567;
 import org.cloudburstmc.protocol.bedrock.codec.v568.Bedrock_v568;
 import org.cloudburstmc.protocol.bedrock.codec.v575.Bedrock_v575;
 import org.cloudburstmc.protocol.bedrock.codec.v582.Bedrock_v582;
-import org.cloudburstmc.protocol.bedrock.codec.v588.Bedrock_v588;
+import org.cloudburstmc.protocol.bedrock.codec.v589.Bedrock_v589;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,7 +61,7 @@ public class BedrockVersionUtils {
      * Default Bedrock codec that should act as a fallback. Should represent the latest available
      * release of the game that Geyser supports.
      */
-    public static final BedrockCodec LATEST_CODEC = Bedrock_v588.CODEC;
+    public static final BedrockCodec LATEST_CODEC = Bedrock_v589.CODEC;
     /**
      * A list of all supported Bedrock versions that can join Geyser
      */
